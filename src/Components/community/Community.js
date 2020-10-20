ec770b7634250af983113a031b41d5e10ba3cf6c
ktplkr-nihon <ashuengi12@gmail.com>
2020-10-20 14:49:38 +0530

    post community group

--- src/Components/community/Community.js
+++ src/Components/community/Community.js
@@ -1,11 +1,44 @@
 import React, {Component} from 'react';
 import './community.css';
 class Community extends Component {
-
+   constructor(){
+      super();
+      this.state = {
+          image:'',
+          comment:'',
+          postc: false
+      }
+   }
+   imageHolder = (event) => {
+   	 this.setState({image:event.target.value})
+   }
+   commentHolder = (event) => {
+   	this.setState({comment:event.target.value})
+   }
+   postSubmit = () => {
+     this.setState({postc:true})
+   }
+   createCard = () => {
+   	var card = [];
+   	card.push(<div className='card'>
+   		<img className='card-img' src={this.state.image}/>
+        <p className='card-comment'>{this.state.comment}</p>
+   	</div>);
+   	return card;
+   }
 	render(){
 		return(
 
-              <div className='Communitygrid'> ok</div>
+              <div className='Communitygrid'>
+                  <div className='post-box'>
+                       <h1 className='h-1'>POST YOUR THOUGHTS!</h1>
+                       	<label className='img-label'>select image</label><br/>
+                       	<input onChange={this.imageHolder}className='img-input' type='file' id='fname' name='fname'/>
+                       	<textarea onChange={this.commentHolder} className='cmmt-input' id="cmmt" name="cmmt" rows="4" cols="50"/>
+                       	<button onClick={this.postSubmit} className='post-submit'>POST</button>
+                       	{this.state.postc === true ? this.createCard() : <span></span>}
+                  </div>
+              </div>
                      
 			);
 	}
--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -25,10 +25,10 @@ class Nav extends Component
            <a className='a1'onClick={() => this.props.onNavClick('home')}>HOME{this.props.navroute === 'home' ? <div className='navlocator'></div> : <div></div> }</a>
            <a className='a1'onClick={() => this.props.onNavClick('community')}>COMMUNITY{this.props.navroute === 'community' ? <div className='navlocator'></div> : <div></div> }</a>
            <a className='a1'onClick={() => this.props.onNavClick('about')}>ABOUT{this.props.navroute === 'about' ? <div className='navlocator'></div> : <div></div> }</a>
-          <div class="dropdown">
+          <div className="dropdown">
   
-  <button class="dropbtn">EXPLORE{this.props.navroute === 'planets'||this.props.navroute === 'galaxies'||this.props.navroute === 'blackhole' ? <div className='navlocator'></div> : <div></div> }</button>
-  <div class="dropdown-content">
+  <button className="dropbtn">EXPLORE{this.props.navroute === 'planets'||this.props.navroute === 'galaxies'||this.props.navroute === 'blackhole' ? <div className='navlocator'></div> : <div></div> }</button>
+  <div className="dropdown-content">
     <a onClick={() => this.props.onNavClick('planets')}>PLANET</a>
     <a onClick={() => this.props.onNavClick('galaxies')}>GALAXIES</a>
     <a onClick={() => this.props.onNavClick('blackhole')}>BLACKHOLE</a>
